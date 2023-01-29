7314aa83142e652370db0045f67ca05299065c82
Gabriel <gabrielblandy@gmail.com>
2023-01-29 17:48:38 -0300

    Aula 03

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'package:client_control/models/client_type.dart';
 import 'package:client_control/models/clients.dart';
+import 'package:client_control/models/types.dart';
 import 'package:client_control/pages/client_types_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'pages/clients_page.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider<ChangeNotifier>(
-    create: (context) => Clients(clients: []),
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => Clients(clients: [])),
+      ChangeNotifierProvider(
+          create: (context) => Types(types: [
+                ClientType(name: 'Platinum', icon: Icons.credit_card),
+                ClientType(name: 'Golden', icon: Icons.card_membership),
+                ClientType(name: 'Titanium', icon: Icons.credit_score),
+                ClientType(name: 'Diamond', icon: Icons.diamond),
+              ])),
+    ],
     child: const MyApp(),
   ));
 }
